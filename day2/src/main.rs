25559c34ad6407ff227988bfdd83e464f51b3238
teedobey <d.obx2010@gmail.com>
2022-12-03 11:02:22 -0700

    Simplifying day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,26 +7,22 @@ fn main() {
 
 fn part1() {
     let mut scores : HashMap<&str, u64> = HashMap::new();
-    scores.insert("X", 1); // rock
-    scores.insert("Y", 2); // paper
-    scores.insert("Z", 3); // scissors
-    scores.insert("A X", 3); // rock-rock
-    scores.insert("B Y", 3); // paper-paper
-    scores.insert("C Z", 3); // scissors-scissors
-    scores.insert("A Y", 6); // rock-paper
-    scores.insert("B Z", 6); // paper-scissors
-    scores.insert("C X", 6); // scissors-rock
-    scores.insert("A Z", 0); // rock-scissor
-    scores.insert("B X", 0); // paper-rock
-    scores.insert("C Y", 0); // scissors-paper
+    scores.insert("A X", 3+1); // rock-rock
+    scores.insert("B Y", 3+2); // paper-paper
+    scores.insert("C Z", 3+3); // scissors-scissors
+    scores.insert("A Y", 6+2); // rock-paper
+    scores.insert("B Z", 6+3); // paper-scissors
+    scores.insert("C X", 6+1); // scissors-rock
+    scores.insert("A Z", 0+3); // rock-scissor
+    scores.insert("B X", 0+1); // paper-rock
+    scores.insert("C Y", 0+2); // scissors-paper
 
     let mut lines = io::stdin().lines();
     let mut total_score : u64 = 0;
 
     while let Some(rline) = lines.next() {
         let line = rline.unwrap();
-        total_score += scores.get(&line[..]).unwrap(); // for game result
-        total_score += scores.get(&line[2..3]).unwrap();
+        total_score += scores.get(&line[..]).unwrap();
     }
 
     println!("Total score: {}", total_score);
@@ -35,18 +31,15 @@ fn part1() {
 fn part2() {
        
     let mut scores : HashMap<&str, u64> = HashMap::new();
-    scores.insert("X", 0); // lose
-    scores.insert("Y", 3); // draw
-    scores.insert("Z", 6); // win
-    scores.insert("A X", 3); // rock-[scissors]
-    scores.insert("B Y", 2); // paper-[paper]
-    scores.insert("C Z", 1); // scissors-[rock]
-    scores.insert("A Y", 1); // rock-[rock]
-    scores.insert("B Z", 3); // paper-[scissors]
-    scores.insert("C X", 2); // scissors-[paper]
-    scores.insert("A Z", 2); // rock-[paper]
-    scores.insert("B X", 1); // paper-[rock]
-    scores.insert("C Y", 3); // scissors-[scissors]
+    scores.insert("A X", 3+0); // rock-[scissors]
+    scores.insert("B Y", 2+3); // paper-[paper]
+    scores.insert("C Z", 1+6); // scissors-[rock]
+    scores.insert("A Y", 1+3); // rock-[rock]
+    scores.insert("B Z", 3+6); // paper-[scissors]
+    scores.insert("C X", 2+0); // scissors-[paper]
+    scores.insert("A Z", 2+6); // rock-[paper]
+    scores.insert("B X", 1+0); // paper-[rock]
+    scores.insert("C Y", 3+3); // scissors-[scissors]
 
     let mut lines = io::stdin().lines();
     let mut total_score : u64 = 0;
@@ -54,9 +47,7 @@ fn part2() {
     while let Some(rline) = lines.next() {
         let line = rline.unwrap();
         total_score += scores.get(&line[..]).unwrap(); 
-        total_score += scores.get(&line[2..3]).unwrap();  // for game result
     }
 
     println!("Total score: {}", total_score); 
 }
-
